956e8492bee03cc85997ced9e2b53cbd7fd6b487
Peter Varga <soadzoor@gmail.com>
2021-06-11 14:35:42 +0200

    Added fading splashscreen

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -5,9 +5,39 @@ export class Main
 		this.init();
 	}
 
-	private init()
+	private async init()
 	{
-		
+		await this.hideSplashScreen();		
+	}
+
+	private hideSplashScreen()
+	{
+		return new Promise<void>((resolve, reject) =>
+		{
+			const splashScreen = document.getElementById("splashScreen");
+
+			if (splashScreen)
+			{
+				const fadeDuration = 1000; // ms
+				splashScreen.style.transition = `opacity ${fadeDuration / 1000}s ease-out`;
+				setTimeout(() =>
+				{
+					if (splashScreen)
+					{
+						splashScreen.style.opacity = "0";
+					}
+					setTimeout(() =>
+					{
+						splashScreen.remove();
+						resolve();
+					}, fadeDuration);
+				}, 2000);
+			}
+			else
+			{
+				reject("HTML element doesn't exist");
+			}
+		});
 	}
 }
 
